6a9884145f1b8f8963bf832282e9d57a7872e1ad
dimitrilahaye <contact@dimitrilahaye.net>
2024-09-30 17:49:47 +0200

    chore(app): change branding

--- front/scripts/pages/Login.js
+++ front/scripts/pages/Login.js
@@ -2,40 +2,43 @@ import Button from "../elements/Button";
 import Page from "./Page.js";
 
 export default class Login extends Page {
-    /** @type {Button} */
-    #loginButton;
+  /** @type {Button} */
+  #loginButton;
 
-    /**
-     *
-     * @param {Element} $main
-     * @param {function(page: Page): void} loginHandler
-     */
-    constructor({$main, loginHandler}) {
-        super($main, 'login');
+  /**
+   *
+   * @param {Element} $main
+   * @param {function(page: Page): void} loginHandler
+   */
+  constructor({ $main, loginHandler }) {
+    super($main, "login");
 
-        this.$page.insertAdjacentHTML('afterbegin', `
-            <h2 class="title is-2">Welcome to Mortal Kompta</h2>
+    this.$page.insertAdjacentHTML(
+      "afterbegin",
+      `
+            <h2 class="title is-2">Welcome to Buddj.app</h2>
             <!-- Form -->
-        `);
+        `
+    );
 
-        this.#loginButton = new Button({
-            classes: 'is-medium is-info',
-            label: 'Login',
-            clickHandler: async () => {
-                if (this.#loginButton.isDisabled) {
-                    return;
-                }
-                await loginHandler(this);
-            },
-        });
-        this.$page.insertAdjacentElement('beforeend', this.#loginButton.$element);
-    }
+    this.#loginButton = new Button({
+      classes: "is-medium is-info",
+      label: "Login",
+      clickHandler: async () => {
+        if (this.#loginButton.isDisabled) {
+          return;
+        }
+        await loginHandler(this);
+      },
+    });
+    this.$page.insertAdjacentElement("beforeend", this.#loginButton.$element);
+  }
 
-    startLoader() {
-        this.#loginButton.startLoader();
-    }
+  startLoader() {
+    this.#loginButton.startLoader();
+  }
 
-    stopLoader() {
-        this.#loginButton.stopLoader();
-    }
+  stopLoader() {
+    this.#loginButton.stopLoader();
+  }
 }
--- front/vite.config.js
+++ front/vite.config.js
@@ -1,37 +1,37 @@
-import {VitePWA} from 'vite-plugin-pwa'
-import {defineConfig} from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-    plugins: [
-        VitePWA({
-            injectRegister: 'inline',
-            manifestFilename: 'manifest.json',
-            manifest: {
-                theme_color: "#f37c3c",
-                background_color: "#000000",
-                icons: [
-                    {
-                        purpose: "maskable",
-                        sizes: "512x512",
-                        src: "/assets/images/icon512_maskable.png",
-                        type: "image/png"
-                    },
-                    {
-                        purpose: "any",
-                        sizes: "512x512",
-                        src: "/assets/images/icon512_rounded.png",
-                        type: "image/png"
-                    }
-                ],
-                orientation: "any",
-                display: "standalone",
-                dir: "auto",
-                lang: "fr",
-                name: "Mortal Kompta",
-                short_name: "MortalKompta",
-                start_url: "/",
-                scope: "/"
-            },
-        })
-    ]
+  plugins: [
+    VitePWA({
+      injectRegister: "inline",
+      manifestFilename: "manifest.json",
+      manifest: {
+        theme_color: "#F942FF",
+        background_color: "#B034C3",
+        icons: [
+          {
+            purpose: "maskable",
+            sizes: "512x512",
+            src: "/assets/images/icon512_maskable.png",
+            type: "image/png",
+          },
+          {
+            purpose: "any",
+            sizes: "512x512",
+            src: "/assets/images/icon512_rounded.png",
+            type: "image/png",
+          },
+        ],
+        orientation: "any",
+        display: "standalone",
+        dir: "auto",
+        lang: "fr",
+        name: "Buddj.app",
+        short_name: "Buddj",
+        start_url: "/",
+        scope: "/",
+      },
+    }),
+  ],
 });
