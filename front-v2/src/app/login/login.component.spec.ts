9a406e1eb8067c0a2c7cfc538b3f6a475758438a
dimitrilahaye <contact@dimitrilahaye.net>
2024-10-06 18:18:57 +0200

    feat(auth): add isAuthenticated mechanism

--- front-v2/src/app/login/login.component.spec.ts
+++ front-v2/src/app/login/login.component.spec.ts
@@ -1,18 +1,22 @@
 import { render, screen } from '@testing-library/angular';
 import { LoginComponent } from './login.component';
+import { AUTHENTICATION_SERVICE } from '../services/authentication/authentication.interface';
+import { TestBed } from '@angular/core/testing';
+import { AuthenticationService } from '../services/authentication/authentication.service';
 
 describe('LoginComponent', () => {
   describe('When we click on login button', () => {
     it('should call login method', async () => {
-      const loginSpy = jasmine.createSpy();
+      const spy = jasmine.createSpyObj('AuthenticationService', ['login']);
       await render(LoginComponent, {
-        on: {
-          login: loginSpy,
-        },
+        providers: [{ provide: AUTHENTICATION_SERVICE, useValue: spy }],
       });
+      const authenticationServiceSpy = TestBed.inject(
+        AUTHENTICATION_SERVICE
+      ) as jasmine.SpyObj<AuthenticationService>;
       const loginButton = screen.getByRole('button', { name: 'Login' });
       loginButton.click();
-      expect(loginSpy).toHaveBeenCalled();
+      expect(authenticationServiceSpy.login).toHaveBeenCalled();
     });
   });
 });
