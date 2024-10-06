493501be32c016fd1ab2ea5ab8d9cc4ffaf6801a
dimitrilahaye <contact@dimitrilahaye.net>
2024-10-06 18:18:57 +0200

    feat(auth): add isAuthenticated mechanism

--- front-v2/src/app/app.routes.ts
+++ front-v2/src/app/app.routes.ts
@@ -1,4 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { authGuard } from './guards/auth.guard';
 
-export const routes: Routes = [{ path: 'login', component: LoginComponent }];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent, canActivate: [authGuard] },
+];
