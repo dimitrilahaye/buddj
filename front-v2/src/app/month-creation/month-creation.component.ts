1fde11d7045212a3ba1f402f9967cc72fda7bfcd
dimitrilahaye <contact@dimitrilahaye.net>
2024-10-07 16:14:58 +0200

    feat(v2): add month-creation page component

---
+++ front-v2/src/app/month-creation/month-creation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-month-creation',
+  standalone: true,
+  imports: [],
+  templateUrl: './month-creation.component.html',
+  styleUrl: './month-creation.component.scss'
+})
+export class MonthCreationComponent {
+
+}
