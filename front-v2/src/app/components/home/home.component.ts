06a3e8aae1982b0c68d5d42c6beb49f8b698eeae
dimitrilahaye <contact@dimitrilahaye.net>
2024-10-08 09:01:50 +0200

    feat(v2): add menu footer

--- front-v2/src/app/components/home/home.component.ts
+++ front-v2/src/app/components/home/home.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { MenuFooterComponent } from '../menu-footer/menu-footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MenuFooterComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
