7b02f09ee2960b2be0cb0a1c97d27371329f3be8
dimitrilahaye <contact@dimitrilahaye.net>
2024-10-08 10:00:42 +0200

    feat(v2): go to month creation page with creation menu

--- front-v2/src/app/components/menu-footer/menu-footer.component.ts
+++ front-v2/src/app/components/menu-footer/menu-footer.component.ts
@@ -1,12 +1,24 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu-footer',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './menu-footer.component.html',
-  styleUrl: './menu-footer.component.scss'
+  styleUrl: './menu-footer.component.scss',
 })
 export class MenuFooterComponent {
+  constructor(private router: Router) {}
 
+  plusOpen = false;
+
+  openPlus() {
+    this.plusOpen = !this.plusOpen;
+  }
+
+  navigateToMonthCreation() {
+    this.router.navigate(['month-creation']);
+  }
 }
