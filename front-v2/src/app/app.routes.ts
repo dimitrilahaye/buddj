59e798aeb823c9cc82570ba6c4f6337b74633194
dimitrilahaye <contact@dimitrilahaye.net>
2024-10-08 07:18:50 +0200

    feat(v2): add month creation route

--- front-v2/src/app/app.routes.ts
+++ front-v2/src/app/app.routes.ts
@@ -2,9 +2,15 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { authGuard } from './guards/auth.guard';
+import { MonthCreationComponent } from './month-creation/month-creation.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent, canActivate: [authGuard] },
+  {
+    path: 'month-creation',
+    component: MonthCreationComponent,
+    canActivate: [authGuard],
+  },
 ];
