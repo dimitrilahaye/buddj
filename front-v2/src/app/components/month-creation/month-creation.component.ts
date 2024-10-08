3ba137eaa07fd4d5b97555eebae61a3631088c54
dimitrilahaye <contact@dimitrilahaye.net>
2024-10-08 10:24:43 +0200

    feat(v2): get template data for month-creation

--- front-v2/src/app/components/month-creation/month-creation.component.ts
+++ front-v2/src/app/components/month-creation/month-creation.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MonthTemplate } from '../../models/monthTemplate.model';
 
 @Component({
   selector: 'app-month-creation',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
   templateUrl: './month-creation.component.html',
   styleUrl: './month-creation.component.scss',
 })
-export class MonthCreationComponent {}
+export class MonthCreationComponent implements OnInit {
+  template: MonthTemplate | null = null;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.data.subscribe((data) => {
+      this.template = data['template'];
+    });
+  }
+}
