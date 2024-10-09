fce552cffcdd78dfa9741b26d626ae531795bac5
dimitrilahaye <contact@dimitrilahaye.net>
2024-10-09 14:26:08 +0200

    feat(v2): remove storybook and add inputs

--- front-v2/src/app/design-system/design-system.module.ts
+++ front-v2/src/app/design-system/design-system.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './button/button.component';
+import { NumberComponent } from './input/number/number.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MonthComponent } from './input/month/month.component';
 
 @NgModule({
-  declarations: [ButtonComponent],
-  imports: [CommonModule],
-  exports: [ButtonComponent],
+  declarations: [ButtonComponent, NumberComponent, MonthComponent],
+  imports: [CommonModule, ReactiveFormsModule],
+  exports: [ButtonComponent, NumberComponent, MonthComponent],
 })
 export class DesignSystemModule {}
