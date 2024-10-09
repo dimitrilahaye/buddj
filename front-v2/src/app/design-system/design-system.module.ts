22ce07451191f471cc8743a57ebada7c4fc1d820
dimitrilahaye <contact@dimitrilahaye.net>
2024-10-10 01:58:30 +0200

    feat(v2): add divider and fix tests

--- front-v2/src/app/design-system/design-system.module.ts
+++ front-v2/src/app/design-system/design-system.module.ts
@@ -7,6 +7,7 @@ import { MonthComponent } from './input/month/month.component';
 import { SlidingModalComponent } from './modal/sliding-modal/sliding-modal.component';
 import { TextComponent } from './input/text/text.component';
 import { ItemComponent } from './item/item/item.component';
+import { DividerComponent } from './divider/divider.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { ItemComponent } from './item/item/item.component';
     TextComponent,
     SlidingModalComponent,
     ItemComponent,
+    DividerComponent,
   ],
   imports: [CommonModule, ReactiveFormsModule],
   exports: [
@@ -25,6 +27,7 @@ import { ItemComponent } from './item/item/item.component';
     TextComponent,
     SlidingModalComponent,
     ItemComponent,
+    DividerComponent,
   ],
 })
 export class DesignSystemModule {}
