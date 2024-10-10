ee1117cf7f3c2e1f73d600f3f5432843910b6c63
dimitrilahaye <contact@dimitrilahaye.net>
2024-10-10 21:34:39 +0200

    fix(v2): fix reset form for creation month

---
+++ front-v2/src/app/models/monthlyBudget.model.ts
@@ -0,0 +1,54 @@
+/* eslint-disable @typescript-eslint/consistent-type-definitions */
+export type MonthlyBudget = {
+  id: string;
+  date: Date;
+  dashboard: Dashboard;
+  account: Account;
+};
+
+export type Dashboard = {
+  account: DashboardAccount;
+  weeks: DashboardWeeks;
+};
+
+export type DashboardAccount = {
+  currentBalance: number;
+  forecastBalance: number;
+};
+
+export type DashboardWeeks = {
+  weeklyBudgets: DashboardWeeklyBudget[];
+  forecastBalance: number;
+};
+
+export type DashboardWeeklyBudget = {
+  weekName: string;
+  currentBalance: number;
+};
+
+export type Account = {
+  id: string;
+  currentBalance: number;
+  outflows: Outflow[];
+  weeklyBudgets: WeeklyBudget[];
+};
+
+export type Outflow = {
+  id: string;
+  amount: number;
+  label: string;
+  isChecked: boolean;
+};
+
+export type WeeklyBudget = {
+  id: string;
+  name: string;
+  expenses: Expense[];
+};
+
+export type Expense = {
+  id: string;
+  amount: number;
+  label: string;
+  isChecked: boolean;
+};
