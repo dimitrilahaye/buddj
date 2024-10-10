d56f7fa60b718fa39e6304d134d5ced148e58cec
dimitrilahaye <contact@dimitrilahaye.net>
2024-10-10 22:35:46 +0200

    feat(v2): add minimalist store

--- front-v2/src/app/components/home/home.component.spec.ts
+++ front-v2/src/app/components/home/home.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { MONTHLY_BUDGETS_STORE } from '../../stores/monthlyBudgets.store.interface';
+import { signal } from '@angular/core';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,9 +10,16 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent]
-    })
-    .compileComponents();
+      imports: [HomeComponent],
+      providers: [
+        {
+          provide: MONTHLY_BUDGETS_STORE,
+          useValue: {
+            getAll: () => signal([]),
+          },
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
