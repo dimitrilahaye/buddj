a11af37a0c5878fb9017eee274839ee9ec059388
dimitrilahaye <contact@dimitrilahaye.net>
2024-10-10 22:35:46 +0200

    feat(v2): add minimalist store

---
+++ front-v2/src/app/stores/monthlyBudgets.store.interface.ts
@@ -0,0 +1,11 @@
+import { InjectionToken, Signal } from '@angular/core';
+import { MonthlyBudget } from '../models/monthlyBudget.model';
+
+export interface MonthlyBudgetsStoreInterface {
+  addMonth(month: MonthlyBudget): void;
+
+  getAll(): Signal<MonthlyBudget[]>;
+}
+
+export const MONTHLY_BUDGETS_STORE =
+  new InjectionToken<MonthlyBudgetsStoreInterface>('MONTHLY_BUDGETS_STORE');
