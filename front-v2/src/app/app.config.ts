c6593dccb6ce0ab61a62daed0ae9921ab22f352c
dimitrilahaye <contact@dimitrilahaye.net>
2024-10-10 23:43:19 +0200

    feat(v2): add toast

--- front-v2/src/app/app.config.ts
+++ front-v2/src/app/app.config.ts
@@ -11,6 +11,7 @@ import { provideServiceWorker } from '@angular/service-worker';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { AuthenticationService } from './services/authentication/authentication.service';
 import { BrowserModule } from '@angular/platform-browser';
+import { provideHotToastConfig } from '@ngxpert/hot-toast';
 import { AUTHENTICATION_SERVICE } from './services/authentication/authentication.service.interface';
 import { MonthTemplatesService } from './services/monthTemplates/monthTemplates.service';
 import { MONTH_TEMPLATES_SERVICE } from './services/monthTemplates/monthTemplates.service.interface';
@@ -30,6 +31,11 @@ export const appConfig: ApplicationConfig = {
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000',
+    }),
+    provideHotToastConfig({
+      position: 'top-center',
+      autoClose: true,
+      dismissible: true,
     }),
     { provide: AUTHENTICATION_SERVICE, useClass: AuthenticationService },
     {
