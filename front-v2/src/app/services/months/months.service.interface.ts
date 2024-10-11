99e6b3838a4cb4e3867cb19036d29b0dcc93f5f8
dimitrilahaye <contact@dimitrilahaye.net>
2024-10-11 18:11:23 +0200

    feat(v2): send to backend outflows checking

--- front-v2/src/app/services/months/months.service.interface.ts
+++ front-v2/src/app/services/months/months.service.interface.ts
@@ -1,12 +1,21 @@
 import { InjectionToken } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Month } from '../../models/month.model';
-import { MonthlyBudget } from '../../models/monthlyBudget.model';
+import { MonthlyBudget, Outflow } from '../../models/monthlyBudget.model';
+
+export interface UpdateOutflowsChecking {
+  currentBalance: number;
+  outflows: Outflow[];
+}
 
 export default interface MonthsServiceInterface {
   createMonth(month: Month): Observable<MonthlyBudget>;
   getUnarchivedMonths(): Observable<MonthlyBudget[]>;
   deleteOutflow(monthId: string, outflowId: string): Observable<MonthlyBudget>;
+  updateOutflowsChecking(
+    monthId: string,
+    data: UpdateOutflowsChecking
+  ): Observable<MonthlyBudget>;
 }
 
 export const MONTHS_SERVICE = new InjectionToken<MonthsServiceInterface>(
