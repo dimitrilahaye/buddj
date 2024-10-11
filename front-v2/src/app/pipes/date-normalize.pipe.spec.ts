72403c5a0199c451ada685631c9de71cbee7f166
dimitrilahaye <contact@dimitrilahaye.net>
2024-10-11 09:26:45 +0200

    feat(v2): header with current month info

---
+++ front-v2/src/app/pipes/date-normalize.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { DateNormalizePipe } from './date-normalize.pipe';
+
+describe('DateNormalizePipe', () => {
+  it('create an instance', () => {
+    const pipe = new DateNormalizePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
