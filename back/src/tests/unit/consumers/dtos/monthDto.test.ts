f837a5251581c3547cfa9311d49612534de8cf86
dimitrilahaye <contact@dimitrilahaye.net>
2024-10-13 12:55:37 +0200

    feat(v2): add initial balance to weekly dashboard

--- back/src/tests/unit/consumers/dtos/monthDto.test.ts
+++ back/src/tests/unit/consumers/dtos/monthDto.test.ts
@@ -2,174 +2,179 @@ import Account from "../../../../core/models/month/account/Account.js";
 import AccountOutflow from "../../../../core/models/month/account/AccountOutflow.js";
 import WeeklyBudget from "../../../../core/models/month/account/WeeklyBudget.js";
 import Month from "../../../../core/models/month/Month.js";
-import expect, {Clock} from "../../../test-helpers.js";
+import expect, { Clock } from "../../../test-helpers.js";
 import monthDto from "../../../../consumers/api/dtos/monthDto.js";
 import WeeklyExpense from "../../../../core/models/month/account/WeeklyExpense.js";
 
 describe("Unit | Consumers | Dtos | monthDto", function () {
-    const monthDate = new Date('2024-01-01');
-    let clock = new Clock();
+  const monthDate = new Date("2024-01-01");
+  let clock = new Clock();
 
-    afterEach(() => {
-        clock.restore();
-    });
+  afterEach(() => {
+    clock.restore();
+  });
 
-    it("should give a month DTO with right data", function () {
-        // given
-        clock.start(monthDate);
-        const monthProps = {
-            id: 'uuid',
-            date: new Date(),
-            account: new Account({
-                id: 'uuid',
-                currentBalance: 2000,
-                outflows: [
-                    new AccountOutflow({
-                        id: 'uuid',
-                        label: 'outlfow',
-                        amount: 10.05,
-                        isChecked: true,
-                    }),
-                ],
-                weeklyBudgets: [
-                    new WeeklyBudget({
-                        id: 'uuid',
-                        name: 'Semaine 1',
-                        initialBalance: 200,
-                        expenses: [
-                            new WeeklyExpense({
-                                id: 'uuid',
-                                label: 'JOW',
-                                amount: 10,
-                                date: new Date(),
-                                isChecked: true,
-                            }),
-                            new WeeklyExpense({
-                                id: 'uuid',
-                                label: 'JOW',
-                                amount: 20,
-                                date: new Date(),
-                                isChecked: false,
-                            }),
-                        ]
-                    }),
-                    new WeeklyBudget({
-                        id: 'uuid',
-                        name: 'Semaine 2',
-                        initialBalance: 200,
-                    }),
-                    new WeeklyBudget({
-                        id: 'uuid',
-                        name: 'Semaine 3',
-                        initialBalance: 200,
-                    }),
-                    new WeeklyBudget({
-                        id: 'uuid',
-                        name: 'Semaine 4',
-                        initialBalance: 200,
-                    }),
-                    new WeeklyBudget({
-                        id: 'uuid',
-                        name: 'Semaine 5',
-                        initialBalance: 200,
-                    }),
-                ],
-            }),
-        };
-        const month = new Month(monthProps);
-
-        const expectedDto = {
+  it("should give a month DTO with right data", function () {
+    // given
+    clock.start(monthDate);
+    const monthProps = {
+      id: "uuid",
+      date: new Date(),
+      account: new Account({
+        id: "uuid",
+        currentBalance: 2000,
+        outflows: [
+          new AccountOutflow({
             id: "uuid",
-            date: new Date('2024-01-01'),
-            dashboard: {
-                account: {
-                    currentBalance: 2000,
-                    forecastBalance: 1010
-                },
-                weeks: {
-                    forecastBalance: 970,
-                    weeklyBudgets: [
-                        {
-                            weekName: "Semaine 1",
-                            currentBalance: 170,
-                        },
-                        {
-                            weekName: "Semaine 2",
-                            currentBalance: 200,
-                        },
-                        {
-                            weekName: "Semaine 3",
-                            currentBalance: 200,
-                        },
-                        {
-                            weekName: "Semaine 4",
-                            currentBalance: 200,
-                        },
-                        {
-                            weekName: "Semaine 5",
-                            currentBalance: 200,
-                        }
-                    ],
-                }
-            },
-            account: {
+            label: "outlfow",
+            amount: 10.05,
+            isChecked: true,
+          }),
+        ],
+        weeklyBudgets: [
+          new WeeklyBudget({
+            id: "uuid",
+            name: "Semaine 1",
+            initialBalance: 200,
+            expenses: [
+              new WeeklyExpense({
                 id: "uuid",
-                currentBalance: 2000,
-                outflows: [
-                    {
-                        id: "uuid",
-                        amount: 10.05,
-                        label: "outlfow",
-                        isChecked: true
-                    }
-                ],
-                weeklyBudgets: [
-                    {
-                        id: "uuid",
-                        name: "Semaine 1",
-                        expenses: [
-                            {
-                                id: 'uuid',
-                                label: 'JOW',
-                                amount: 10,
-                                isChecked: true,
-                            },
-                            {
-                                id: 'uuid',
-                                label: 'JOW',
-                                amount: 20,
-                                isChecked: false,
-                            },
-                        ],
-                    },
-                    {
-                        id: "uuid",
-                        name: "Semaine 2",
-                        expenses: [],
-                    },
-                    {
-                        id: "uuid",
-                        name: "Semaine 3",
-                        expenses: [],
-                    },
-                    {
-                        id: "uuid",
-                        name: "Semaine 4",
-                        expenses: [],
-                    },
-                    {
-                        id: "uuid",
-                        name: "Semaine 5",
-                        expenses: [],
-                    }
-                ]
-            }
-        };
+                label: "JOW",
+                amount: 10,
+                date: new Date(),
+                isChecked: true,
+              }),
+              new WeeklyExpense({
+                id: "uuid",
+                label: "JOW",
+                amount: 20,
+                date: new Date(),
+                isChecked: false,
+              }),
+            ],
+          }),
+          new WeeklyBudget({
+            id: "uuid",
+            name: "Semaine 2",
+            initialBalance: 200,
+          }),
+          new WeeklyBudget({
+            id: "uuid",
+            name: "Semaine 3",
+            initialBalance: 200,
+          }),
+          new WeeklyBudget({
+            id: "uuid",
+            name: "Semaine 4",
+            initialBalance: 200,
+          }),
+          new WeeklyBudget({
+            id: "uuid",
+            name: "Semaine 5",
+            initialBalance: 200,
+          }),
+        ],
+      }),
+    };
+    const month = new Month(monthProps);
 
-        // when
-        const dto = monthDto(month);
+    const expectedDto = {
+      id: "uuid",
+      date: new Date("2024-01-01"),
+      dashboard: {
+        account: {
+          currentBalance: 2000,
+          forecastBalance: 1010,
+        },
+        weeks: {
+          forecastBalance: 970,
+          weeklyBudgets: [
+            {
+              weekName: "Semaine 1",
+              initialBalance: 200,
+              currentBalance: 170,
+            },
+            {
+              weekName: "Semaine 2",
+              initialBalance: 200,
+              currentBalance: 200,
+            },
+            {
+              weekName: "Semaine 3",
+              initialBalance: 200,
+              currentBalance: 200,
+            },
+            {
+              weekName: "Semaine 4",
+              initialBalance: 200,
+              currentBalance: 200,
+            },
+            {
+              weekName: "Semaine 5",
+              initialBalance: 200,
+              currentBalance: 200,
+            },
+          ],
+        },
+      },
+      account: {
+        id: "uuid",
+        currentBalance: 2000,
+        outflows: [
+          {
+            id: "uuid",
+            amount: 10.05,
+            label: "outlfow",
+            isChecked: true,
+          },
+        ],
+        weeklyBudgets: [
+          {
+            id: "uuid",
+            name: "Semaine 1",
+            expenses: [
+              {
+                id: "uuid",
+                label: "JOW",
+                amount: 10,
+                isChecked: true,
+              },
+              {
+                id: "uuid",
+                label: "JOW",
+                amount: 20,
+                isChecked: false,
+              },
+            ],
+          },
+          {
+            id: "uuid",
+            name: "Semaine 2",
+            expenses: [],
+          },
+          {
+            id: "uuid",
+            name: "Semaine 3",
+            expenses: [],
+          },
+          {
+            id: "uuid",
+            name: "Semaine 4",
+            expenses: [],
+          },
+          {
+            id: "uuid",
+            name: "Semaine 5",
+            expenses: [],
+          },
+        ],
+      },
+    };
 
-        // then
-        expect(dto).to.deep.equal(expectedDto);
-    });
+    // when
+    const dto = monthDto(month);
+
+    // then
+    expect(dto).to.deep.equal(expectedDto);
+  });
 });
--- back/src/tests/unit/core/models/month/dashboard/WeeklyBudgetDashboard.test.ts
+++ back/src/tests/unit/core/models/month/dashboard/WeeklyBudgetDashboard.test.ts
@@ -3,109 +3,111 @@ import WeeklyBudgetDashboard from "../../../../../../core/models/dashboard/Weekl
 import WeeklyBudget from "../../../../../../core/models/month/account/WeeklyBudget.js";
 import WeeklyExpense from "../../../../../../core/models/month/account/WeeklyExpense.js";
 
-describe('Unit | Core | Models | Dashboard | WeeklyBudgetDashboard', () => {
-   describe('#constructor', () => {
-      it('should return a weekly budget dashboard with right data when current balance is greater than 0', () => {
-         // given
-          const props = {
-              weekName: 'Semaine 1',
-              initialBalance: 200,
-              weeklyBudget: new WeeklyBudget({
-                  id: 'uuid',
-                  initialBalance: 200,
-                  name: 'Semaine 1',
-                  expenses: [
-                      new WeeklyExpense({
-                          id: 'uuid',
-                          label: 'JOW',
-                          date: new Date(),
-                          amount: 10,
-                          isChecked: true,
-                      }),
-                      new WeeklyExpense({
-                          id: 'uuid',
-                          label: 'JOW',
-                          date: new Date(),
-                          amount: 20,
-                          isChecked: false,
-                      }),
-                      new WeeklyExpense({
-                          id: 'uuid',
-                          label: 'JOW',
-                          date: new Date(),
-                          amount: 20,
-                          isChecked: true,
-                      }),
-                      new WeeklyExpense({
-                          id: 'uuid',
-                          label: 'JOW',
-                          date: new Date(),
-                          amount: 10,
-                          isChecked: false,
-                      }),
-                  ],
-              }),
-          };
+describe("Unit | Core | Models | Dashboard | WeeklyBudgetDashboard", () => {
+  describe("#constructor", () => {
+    it("should return a weekly budget dashboard with right data when current balance is greater than 0", () => {
+      // given
+      const props = {
+        weekName: "Semaine 1",
+        initialBalance: 200,
+        weeklyBudget: new WeeklyBudget({
+          id: "uuid",
+          initialBalance: 200,
+          name: "Semaine 1",
+          expenses: [
+            new WeeklyExpense({
+              id: "uuid",
+              label: "JOW",
+              date: new Date(),
+              amount: 10,
+              isChecked: true,
+            }),
+            new WeeklyExpense({
+              id: "uuid",
+              label: "JOW",
+              date: new Date(),
+              amount: 20,
+              isChecked: false,
+            }),
+            new WeeklyExpense({
+              id: "uuid",
+              label: "JOW",
+              date: new Date(),
+              amount: 20,
+              isChecked: true,
+            }),
+            new WeeklyExpense({
+              id: "uuid",
+              label: "JOW",
+              date: new Date(),
+              amount: 10,
+              isChecked: false,
+            }),
+          ],
+        }),
+      };
 
-         // when
-          const weekly = new WeeklyBudgetDashboard(props);
+      // when
+      const weekly = new WeeklyBudgetDashboard(props);
 
-         // then
-          expect(weekly).to.deep.equal({
-              weekName: props.weekName,
-              currentBalance: 140,
-          });
+      // then
+      expect(weekly).to.deep.equal({
+        weekName: props.weekName,
+        initialBalance: 200,
+        currentBalance: 140,
       });
-      it('should return a weekly budget dashboard with right data when current balance is negative', () => {
-         // given
-          const props = {
-              weekName: 'Semaine 1',
-              initialBalance: 200,
-              weeklyBudget: new WeeklyBudget({
-                  id: 'uuid',
-                  initialBalance: 200,
-                  name: 'Semaine 1',
-                  expenses: [
-                      new WeeklyExpense({
-                          id: 'uuid',
-                          label: 'JOW',
-                          date: new Date(),
-                          amount: 210,
-                          isChecked: true,
-                      }),
-                      new WeeklyExpense({
-                          id: 'uuid',
-                          label: 'JOW',
-                          date: new Date(),
-                          amount: 20,
-                          isChecked: true,
-                      }),
-                      new WeeklyExpense({
-                          id: 'uuid',
-                          label: 'JOW',
-                          date: new Date(),
-                          amount: 20,
-                          isChecked: false,
-                      }),
-                      new WeeklyExpense({
-                          id: 'uuid',
-                          label: 'JOW',
-                          date: new Date(),
-                          amount: 10,
-                          isChecked: false,
-                      }),
-                  ],
-              }),
-          };
+    });
+    it("should return a weekly budget dashboard with right data when current balance is negative", () => {
+      // given
+      const props = {
+        weekName: "Semaine 1",
+        initialBalance: 200,
+        weeklyBudget: new WeeklyBudget({
+          id: "uuid",
+          initialBalance: 200,
+          name: "Semaine 1",
+          expenses: [
+            new WeeklyExpense({
+              id: "uuid",
+              label: "JOW",
+              date: new Date(),
+              amount: 210,
+              isChecked: true,
+            }),
+            new WeeklyExpense({
+              id: "uuid",
+              label: "JOW",
+              date: new Date(),
+              amount: 20,
+              isChecked: true,
+            }),
+            new WeeklyExpense({
+              id: "uuid",
+              label: "JOW",
+              date: new Date(),
+              amount: 20,
+              isChecked: false,
+            }),
+            new WeeklyExpense({
+              id: "uuid",
+              label: "JOW",
+              date: new Date(),
+              amount: 10,
+              isChecked: false,
+            }),
+          ],
+        }),
+      };
 
-         // when
-          const weekly = new WeeklyBudgetDashboard(props);
+      // when
+      const weekly = new WeeklyBudgetDashboard(props);
 
-         // then
-          expect(weekly).to.deep.equal({
-              weekName: props.weekName,
-              currentBalance: -60,
-          });
+      // then
+      expect(weekly).to.deep.equal({
+        weekName: props.weekName,
+        initialBalance: 200,
+        currentBalance: -60,
       });
-   });
+    });
+  });
 });
