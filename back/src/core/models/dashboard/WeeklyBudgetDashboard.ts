4d767b40392c4279e66de3aaab98bbd5ea13b813
dimitrilahaye <contact@dimitrilahaye.net>
2024-10-13 12:55:37 +0200

    feat(v2): add initial balance to weekly dashboard

--- back/src/core/models/dashboard/WeeklyBudgetDashboard.ts
+++ back/src/core/models/dashboard/WeeklyBudgetDashboard.ts
@@ -1,13 +1,22 @@
 import WeeklyBudget from "../month/account/WeeklyBudget.js";
 
 export default class WeeklyBudgetDashboard {
+  weekName: string;
+  currentBalance: number;
+  initialBalance: number;
+
+  constructor(props: {
     weekName: string;
-    currentBalance: number;
+    initialBalance: number;
+    weeklyBudget: WeeklyBudget;
+  }) {
+    this.weekName = props.weekName;
 
-    constructor(props: { weekName: string, initialBalance: number, weeklyBudget: WeeklyBudget }) {
-        this.weekName = props.weekName;
-
-        const sumOfAllExpensesAmount = props.weeklyBudget.expenses.reduce((prev, curr) => prev + curr.amount, 0);
-        this.currentBalance = props.initialBalance - sumOfAllExpensesAmount;
-    }
+    const sumOfAllExpensesAmount = props.weeklyBudget.expenses.reduce(
+      (prev, curr) => prev + curr.amount,
+      0
+    );
+    this.initialBalance = props.initialBalance;
+    this.currentBalance = this.initialBalance - sumOfAllExpensesAmount;
+  }
 }
