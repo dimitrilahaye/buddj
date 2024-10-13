7ad43b34b2481ebec686d4a578152d5d5b7aaa31
dimitrilahaye <contact@dimitrilahaye.net>
2024-10-13 17:17:09 +0200

    feat(v2): add info component

---
+++ front-v2/src/app/design-system/info/info.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { InfoComponent } from './info.component';
+import { DesignSystemModule } from '../design-system.module';
+
+describe('InfoComponent', () => {
+  let component: InfoComponent;
+  let fixture: ComponentFixture<InfoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [InfoComponent],
+      imports: [DesignSystemModule],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(InfoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
