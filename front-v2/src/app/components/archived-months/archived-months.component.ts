2d479e2c295d48fd78df701beaaa79e403a6d0ec
dimitrilahaye <contact@dimitrilahaye.net>
2024-10-13 03:19:39 +0200

    feat(v2): add archives page

---
+++ front-v2/src/app/components/archived-months/archived-months.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-archived-months',
+  standalone: true,
+  imports: [],
+  templateUrl: './archived-months.component.html',
+  styleUrl: './archived-months.component.scss'
+})
+export class ArchivedMonthsComponent {
+
+}
