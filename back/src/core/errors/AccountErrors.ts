fce3f3c49b9e25a8e4f7697ed0b4b9c73fd845b5
dimitrilahaye <contact@dimitrilahaye.net>
2024-10-20 22:14:45 +0200

    sr(back): reformat AccountErrors

--- back/src/core/errors/AccountErrors.ts
+++ back/src/core/errors/AccountErrors.ts
@@ -1,22 +1,26 @@
 class AccountInitialBalanceError extends Error {
-    constructor() {
-        super('Account: initial balance should be greater than 0');
-        this.name = 'AccountInitialBalanceError';
-    }
+  constructor() {
+    super("Account: initial balance should be greater than 0");
+    this.name = "AccountInitialBalanceError";
+  }
 }
 
 class AccountWeeklyBudgetsError extends Error {
-    constructor() {
-        super('Account: should contain 5 weekly budgets');
-        this.name = 'AccountWeeklyBudgetsError';
-    }
+  constructor() {
+    super("Account: should contain 5 weekly budgets");
+    this.name = "AccountWeeklyBudgetsError";
+  }
 }
 
 class AccountOutflowsError extends Error {
-    constructor() {
-        super('Account: should contain at least one outflow');
-        this.name = 'AccountOutflowsError';
-    }
+  constructor() {
+    super("Account: should contain at least one outflow");
+    this.name = "AccountOutflowsError";
+  }
 }
 
-export {AccountInitialBalanceError, AccountOutflowsError, AccountWeeklyBudgetsError};
+export {
+  AccountInitialBalanceError,
+  AccountOutflowsError,
+  AccountWeeklyBudgetsError,
+};
