1876983209757dbb8eda8eb1a6e5ec4225e81ff7
dimitrilahaye <contact@dimitrilahaye.net>
2024-10-21 07:42:09 +0200

    feat(v2): add transfer modal to expenses page

---
+++ front-v2/src/app/components/transfer-modals/transfer-choice/transfer-choice.component.spec.ts
@@ -0,0 +1,34 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TransferChoiceComponent } from './transfer-choice.component';
+import { CurrencyPipe } from '@angular/common';
+import { MONTHLY_BUDGETS_STORE } from '../../../stores/monthlyBudgets.store.interface';
+import { signal } from '@angular/core';
+
+describe('TransferChoiceComponent', () => {
+  let component: TransferChoiceComponent;
+  let fixture: ComponentFixture<TransferChoiceComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TransferChoiceComponent],
+      providers: [
+        {
+          provide: MONTHLY_BUDGETS_STORE,
+          useValue: {
+            getCurrent: () => signal(null),
+          },
+        },
+        CurrencyPipe,
+      ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TransferChoiceComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ front-v2/src/app/components/transfer-modals/transfer-choice/transfer-choice.component.ts
@@ -0,0 +1,203 @@
+import {
+  Component,
+  Inject,
+  Input,
+  OnInit,
+  output,
+  signal,
+  Signal,
+} from '@angular/core';
+import { DesignSystemModule } from '../../../design-system/design-system.module';
+import {
+  Account,
+  DashboardWeeklyBudget,
+  MonthlyBudget,
+  WeeklyBudget,
+} from '../../../models/monthlyBudget.model';
+import {
+  MONTHLY_BUDGETS_STORE,
+  MonthlyBudgetsStoreInterface,
+} from '../../../stores/monthlyBudgets.store.interface';
+import { CommonModule, CurrencyPipe } from '@angular/common';
+import { TransferChoiceValidationComponent } from '../transfer-choice-validation/transfer-choice-validation.component';
+
+interface ValidationData {
+  data: Account | (DashboardWeeklyBudget & { id: string });
+  type: 'Account' | 'WeeklyBudget';
+  information: string;
+}
+
+@Component({
+  selector: 'app-transfer-choice',
+  standalone: true,
+  imports: [
+    DesignSystemModule,
+    CurrencyPipe,
+    CommonModule,
+    TransferChoiceValidationComponent,
+  ],
+  providers: [CurrencyPipe],
+  templateUrl: './transfer-choice.component.html',
+  styleUrl: './transfer-choice.component.scss',
+})
+export class TransferChoiceComponent implements OnInit {
+  @Input()
+  openMenuModal = false;
+  @Input()
+  fromAccount: Account | null = null;
+  @Input()
+  fromWeeklyBudget: WeeklyBudget | null = null;
+  toggleModal = output<Event>();
+  submitted = output<{
+    monthId: string;
+    fromType: 'account' | 'weekly-budget';
+    fromId: string;
+    toType: 'account' | 'weekly-budget';
+    toId: string;
+  }>();
+
+  transferChoiceModalIsOpen = false;
+  currentMonth: Signal<MonthlyBudget | null> = signal(null);
+  validationData: ValidationData | null = null;
+  fromValidationAccount: Account | null = null;
+  fromValidationWeeklyBudget: WeeklyBudget | null = null;
+
+  constructor(
+    @Inject(MONTHLY_BUDGETS_STORE)
+    private monthlyBudgetStore: MonthlyBudgetsStoreInterface,
+    private currencyPipe: CurrencyPipe
+  ) {}
+
+  ngOnInit(): void {
+    this.currentMonth = this.monthlyBudgetStore.getCurrent();
+  }
+
+  get transferAmount() {
+    if (this.fromAccount) {
+      return this.fromAccount.currentBalance;
+    }
+    if (this.fromWeeklyBudget) {
+      const fromWeeklyBudget =
+        this.currentMonth()!.dashboard.weeks.weeklyBudgets.find(
+          (w) => w.weekName === this.fromWeeklyBudget!.name
+        );
+      return fromWeeklyBudget?.currentBalance;
+    }
+    throw new Error('You need to set fromAccount or fromWeeklyBudget Input');
+  }
+
+  get transferSourceLabel() {
+    if (this.fromAccount) {
+      return 'Solde courant';
+    }
+    if (this.fromWeeklyBudget) {
+      return this.fromWeeklyBudget.name;
+    }
+    throw new Error('You need to set fromAccount or fromWeeklyBudget Input');
+  }
+
+  get buttons(): ValidationData[] {
+    if (this.fromAccount) {
+      return this.currentMonth()!.dashboard.weeks.weeklyBudgets.map((w) => {
+        const week = this.currentMonth()?.account.weeklyBudgets.find(
+          (week) => week.name === w.weekName
+        );
+        return {
+          data: { ...w, id: week!.id },
+          type: 'Account',
+          information: this.getWeekTransferInformation(w),
+        };
+      });
+    }
+    if (this.fromWeeklyBudget) {
+      const otherWeeklyBudgets = this.currentMonth()!
+        .dashboard.weeks.weeklyBudgets.filter(
+          (w) => w.weekName !== this.fromWeeklyBudget?.name
+        )
+        .map((w) => {
+          const week = this.currentMonth()?.account.weeklyBudgets.find(
+            (week) => week.name === w.weekName
+          );
+          return {
+            data: { ...w, id: week!.id },
+            type: 'WeeklyBudget',
+            information: this.getWeekTransferInformation(w),
+          };
+        })
+        .sort((a, b) => a.data.weekName.localeCompare(b.data.weekName));
+      return [
+        ...(otherWeeklyBudgets as ValidationData[]),
+        {
+          data: this.currentMonth()!.account,
+          type: 'Account',
+          information: this.getAccountTransfertInformation(
+            this.currentMonth()!.account
+          ),
+        },
+      ];
+    }
+    throw new Error('You need to set fromAccount or fromWeeklyBudget Input');
+  }
+
+  private getWeekTransferInformation(weeklyBudget: DashboardWeeklyBudget) {
+    return (
+      weeklyBudget.weekName +
+      ` (${this.currencyPipe.transform(
+        weeklyBudget.currentBalance,
+        'EUR'
+      )} » ${this.currencyPipe.transform(
+        weeklyBudget.currentBalance + this.transferAmount!,
+        'EUR'
+      )})`
+    );
+  }
+
+  private getAccountTransfertInformation(account: Account) {
+    return `Solde courant (${this.currencyPipe.transform(
+      account.currentBalance,
+      'EUR'
+    )} » ${this.currencyPipe.transform(
+      account.currentBalance + this.transferAmount!,
+      'EUR'
+    )})`;
+  }
+
+  closeTransferChoiceModal(event: Event) {
+    this.transferChoiceModalIsOpen = false;
+    event.stopPropagation();
+  }
+
+  openTransferChoiceModal(
+    button: {
+      data: Account | (DashboardWeeklyBudget & { id: string });
+      type: 'Account' | 'WeeklyBudget';
+      information: string;
+    },
+    event: Event
+  ) {
+    this.transferChoiceModalIsOpen = true;
+    this.validationData = button;
+    if (this.fromAccount) {
+      this.fromValidationAccount = this.fromAccount;
+    }
+    if (this.fromWeeklyBudget) {
+      this.fromValidationWeeklyBudget = this.fromWeeklyBudget;
+    }
+    this.toggle(event);
+  }
+
+  toggle(event: Event) {
+    this.toggleModal.emit(event);
+    event.stopPropagation();
+  }
+
+  submitTransfert(data: {
+    monthId: string;
+    fromType: 'account' | 'weekly-budget';
+    fromId: string;
+    toType: 'account' | 'weekly-budget';
+    toId: string;
+  }) {
+    this.submitted.emit(data);
+  }
+}
