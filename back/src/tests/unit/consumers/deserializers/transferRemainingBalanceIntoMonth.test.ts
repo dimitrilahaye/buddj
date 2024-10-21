9fac3d568ea0e784e89dff0b8f98c4be7fcbe833
dimitrilahaye <contact@dimitrilahaye.net>
2024-10-21 04:47:40 +0200

    feat(back): add deserializer

---
+++ back/src/tests/unit/consumers/deserializers/transferRemainingBalanceIntoMonth.test.ts
@@ -0,0 +1,70 @@
+import expect from "../../../test-helpers.js";
+import deserializer from "../../../../consumers/api/deserializers/transferRemainingBalanceIntoMonth.js";
+
+describe.only("Unit | Consumers | deserializers | transferRemainingBalanceIntoMonth", function () {
+  describe("from weekly budget to account", function () {
+    const params = {
+      monthId: "ebf6aae9-5664-4e40-974f-9b373524d031",
+      fromType: "weekly-budget",
+      fromId: "ebf6aae9-5664-4e40-974f-9b373524d031",
+      toType: "account",
+      toId: "ebf6aae9-5664-4e40-974f-9b373524d031",
+    };
+
+    it("should return a right command", async function () {
+      // when
+      const command = deserializer(params);
+
+      // then
+      expect(command).to.deep.equal({
+        monthId: params.monthId,
+        fromWeeklyBudgetId: params.fromId,
+        toAccountId: params.toId,
+      });
+    });
+  });
+
+  describe("from weekly budget to weekly budget", function () {
+    const params = {
+      monthId: "ebf6aae9-5664-4e40-974f-9b373524d031",
+      fromType: "weekly-budget",
+      fromId: "ebf6aae9-5664-4e40-974f-9b373524d031",
+      toType: "weekly-budget",
+      toId: "ebf6aae9-5664-4e40-974f-9b373524d031",
+    };
+
+    it("should return a right command", async function () {
+      // when
+      const command = deserializer(params);
+
+      // then
+      expect(command).to.deep.equal({
+        monthId: params.monthId,
+        fromWeeklyBudgetId: params.fromId,
+        toWeeklyBudgetId: params.toId,
+      });
+    });
+  });
+
+  describe("from account to weekly budget", function () {
+    const params = {
+      monthId: "ebf6aae9-5664-4e40-974f-9b373524d031",
+      fromType: "account",
+      fromId: "ebf6aae9-5664-4e40-974f-9b373524d031",
+      toType: "weekly-budget",
+      toId: "ebf6aae9-5664-4e40-974f-9b373524d031",
+    };
+
+    it("should return a right command", async function () {
+      // when
+      const command = deserializer(params);
+
+      // then
+      expect(command).to.deep.equal({
+        monthId: params.monthId,
+        fromAccountId: params.fromId,
+        toWeeklyBudgetId: params.toId,
+      });
+    });
+  });
+});
