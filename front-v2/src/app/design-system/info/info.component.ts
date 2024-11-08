f6f3413a0d11c3207eaeddbcfc57997af4e2c9c8
Dimitri Lahaye <contact@dimitrilahaye.net>
2024-11-08 14:08:36 +0100

    fix(front): add standalone false

--- front-v2/src/app/design-system/info/info.component.ts
+++ front-v2/src/app/design-system/info/info.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
+  standalone: false,
   selector: 'app-info',
   templateUrl: './info.component.html',
   styleUrl: './info.component.scss',
--- front-v2/src/app/design-system/modal/modal/modal.component.ts
+++ front-v2/src/app/design-system/modal/modal/modal.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, output } from '@angular/core';
 
 @Component({
+  standalone: false,
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.scss',
