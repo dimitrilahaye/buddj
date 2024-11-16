ba6fe1b40845c05722f09aab2f6c981118048b79
Dimitri Lahaye <contact@dimitrilahaye.net>
2024-11-16 22:08:01 +0100

    style(back): lint for CreateNewMonth usecase

--- back/src/core/usecases/CreateNewMonth.ts
+++ back/src/core/usecases/CreateNewMonth.ts
@@ -3,13 +3,13 @@ import MonthFactory from "../factories/MonthFactory.js";
 import MonthCreationCommand from "../commands/MonthCreationCommand.js";
 
 export default class CreateNewMonth {
-    constructor(
-        public monthRepository: MonthRepository,
-        public monthFactory: MonthFactory,
-    ) {}
+  constructor(
+    public monthRepository: MonthRepository,
+    public monthFactory: MonthFactory
+  ) {}
 
-    async execute(command: MonthCreationCommand) {
-        const month = this.monthFactory.create(command);
-        return await this.monthRepository.save(month);
-    }
+  async execute(command: MonthCreationCommand) {
+    const month = this.monthFactory.create(command);
+    return await this.monthRepository.save(month);
+  }
 }
