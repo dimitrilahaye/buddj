6e3441d278d2190387d05e6b7ad719e15c208ad2
Dimitri Lahaye <contact@dimitrilahaye.net>
2024-11-24 12:16:15 +0100

    feat(back): add GetYearlyOutflows usecase and dto

---
+++ back/src/core/usecases/GetYearlyOutflows.ts
@@ -0,0 +1,9 @@
+import YearlyOutflowRepository from "../ports/repositories/YearlyOutflowRepository.js";
+
+export default class GetYearlyOutflows {
+  constructor(public yearlyOutflowRepository: YearlyOutflowRepository) {}
+
+  async execute() {
+    return this.yearlyOutflowRepository.getAll();
+  }
+}
