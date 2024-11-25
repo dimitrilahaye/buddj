912285bb861c40abe185bef02e0fdc99f85cc600
Dimitri Lahaye <contact@dimitrilahaye.net>
2024-11-25 17:50:38 +0100

    feat(front): delete a yearly outflow

--- front-v2/src/app/components/yearly-outflows/yearly-outflows.component.ts
+++ front-v2/src/app/components/yearly-outflows/yearly-outflows.component.ts
@@ -3,10 +3,20 @@ import {
   YEARLY_OUTFLOWS_STORE,
   YearlyOutflowsStoreInterface,
 } from '../../stores/yearlyOutflows/yearlyOutflows.store.interface';
-import { YearlyOutflows } from '../../models/yearlyOutflow.model';
+import {
+  YearlyOutflow,
+  YearlyOutflows,
+} from '../../models/yearlyOutflow.model';
 import { CommonModule } from '@angular/common';
 import { DesignSystemModule } from '../../design-system/design-system.module';
 import { Router } from '@angular/router';
+import YearlyOutflowsServiceInterface, {
+  YEARLY_OUTFLOWS_SERVICE,
+} from '../../services/yearlyOutflows/yearly-outflows.service.interface';
+import { finalize } from 'rxjs';
+import ToasterServiceInterface, {
+  TOASTER_SERVICE,
+} from '../../services/toaster/toaster.service.interface';
 
 @Component({
   selector: 'app-yearly-outflows',
@@ -17,17 +27,51 @@ import { Router } from '@angular/router';
 })
 export class YearlyOutflowsComponent {
   outflows: Signal<YearlyOutflows | null>;
+  removeIsLoading = false;
+  outflowDelationModalIsOpen = false;
+  outflowToDelete: YearlyOutflow | null = null;
 
   constructor(
-    private router: Router,
+    private readonly router: Router,
     @Inject(YEARLY_OUTFLOWS_STORE)
-    private readonly yearlyOutflowsStore: YearlyOutflowsStoreInterface
+    private readonly yearlyOutflowsStore: YearlyOutflowsStoreInterface,
+    @Inject(YEARLY_OUTFLOWS_SERVICE)
+    private readonly yearlyOutflowsService: YearlyOutflowsServiceInterface,
+    @Inject(TOASTER_SERVICE) private readonly toaster: ToasterServiceInterface
   ) {
     this.outflows = this.yearlyOutflowsStore.getAll();
   }
 
   getOutflowsForMonth(month: number) {
     return this.outflows()![month];
+  }
+
+  openOutflowDelationModal(outflow: YearlyOutflow, event: Event) {
+    this.outflowDelationModalIsOpen = true;
+    this.outflowToDelete = outflow;
+    event.stopPropagation();
+  }
+
+  closeOutflowDelationModal(event: Event) {
+    this.outflowDelationModalIsOpen = false;
+    this.outflowToDelete = null;
+    event.stopPropagation();
+  }
+
+  removeOutflow(event: Event) {
+    this.removeIsLoading = true;
+    event.stopPropagation();
+    this.yearlyOutflowsService
+      .remove(this.outflowToDelete!.id)
+      .pipe(
+        finalize(() => {
+          this.removeIsLoading = false;
+          this.outflowDelationModalIsOpen = false;
+        })
+      )
+      .subscribe(() => {
+        this.toaster.success('La sortie annuelle ont été supprimée !');
+      });
   }
 
   backToHome() {
