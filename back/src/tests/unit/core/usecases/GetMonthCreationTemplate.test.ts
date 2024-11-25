bf68ab411215a75545719c4372ba277362699f6c
Dimitri Lahaye <contact@dimitrilahaye.net>
2024-11-25 20:10:19 +0100

    feat(back): add monthly project in get creation template usecase

--- back/src/tests/unit/core/usecases/GetMonthCreationTemplate.test.ts
+++ back/src/tests/unit/core/usecases/GetMonthCreationTemplate.test.ts
@@ -1,26 +1,52 @@
 import expect from "../../../test-helpers.js";
-import MonthCreationTemplateRepository from "../../../../providers/persistence/repositories/MonthCreationTemplateRepository.js";
-import { pendingDebitRepositoryStub } from "./test-helpers.js";
-import MonthCreationTemplate from "../../../../core/models/template/MonthCreationTemplate.js";
+import {
+  monthCreationTemplateRepositoryStub,
+  pendingDebitRepositoryStub,
+  resetStubs,
+  yearlyOutflowRepositoryStub,
+} from "./test-helpers.js";
 import GetMonthCreationTemplate from "../../../../core/usecases/GetMonthCreationTemplate.js";
+import { after } from "mocha";
+import sinon from "sinon";
 
 describe("Unit | Core | Usecases | GetMonthCreationTemplate", function () {
+  after(() => {
+    resetStubs();
+  });
+
   it("should return data for a new month creation", async function () {
     // given
-    const usecase = new GetMonthCreationTemplate(
-      new MonthCreationTemplateRepository(),
-      pendingDebitRepositoryStub
-    );
     const expectedDebits = Symbol("debits");
     pendingDebitRepositoryStub.getAll.resolves(expectedDebits);
+
+    const expectedTotal = 120;
+    const expectedOutflows = {
+      getMonthlyProjectsAmount: () => expectedTotal,
+    };
+    yearlyOutflowRepositoryStub.getAll.resolves(expectedOutflows);
+
+    const expectedTemplate = {
+      addMonthlyProjectForAmount: sinon.stub(),
+    };
+    monthCreationTemplateRepositoryStub.getNewMonthTemplate.resolves(
+      expectedTemplate
+    );
+
+    const usecase = new GetMonthCreationTemplate(
+      monthCreationTemplateRepositoryStub,
+      pendingDebitRepositoryStub,
+      yearlyOutflowRepositoryStub
+    );
 
     // when
     const monthCreationTemplate = await usecase.execute();
 
     // then
-    expect(monthCreationTemplate.template).to.be.instanceof(
-      MonthCreationTemplate
-    );
+    expect(monthCreationTemplateRepositoryStub.getNewMonthTemplate).to.have.been
+      .calledOnce;
+    expect(
+      expectedTemplate.addMonthlyProjectForAmount
+    ).to.have.been.calledOnceWith(expectedTotal);
     expect(monthCreationTemplate.pendingDebits).to.be.equals(expectedDebits);
   });
 });
