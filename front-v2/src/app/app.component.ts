4b3bad7fda51d3d06040d53bc639c33e5e951404
Dimitri Lahaye <contact@dimitrilahaye.net>
2024-12-10 18:55:14 +0100

    feat(front): add scroll to top

--- front-v2/src/app/app.component.ts
+++ front-v2/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DesignSystemModule } from './design-system/design-system.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DesignSystemModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
