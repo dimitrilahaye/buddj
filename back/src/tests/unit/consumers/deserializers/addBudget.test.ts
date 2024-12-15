de1035e5170182435b3174134739ff01402f478b
Dimitri Lahaye <contact@dimitrilahaye.net>
2024-12-15 17:38:26 +0100

    feat(back): add addBudget consumers

---
+++ back/src/tests/unit/consumers/deserializers/addBudget.test.ts
@@ -0,0 +1,24 @@
+import expect from "../../../test-helpers.js";
+import deserializer from "../../../../consumers/api/deserializers/addBudget.js";
+
+describe("Unit | Consumers | deserializers | addBudget", function () {
+  const body = {
+    name: "JOW",
+    initialBalance: 10,
+  };
+  const params = {
+    monthId: "ebf6aae9-5664-4e40-974f-9b373524d031",
+  };
+
+  it("should return a right command", async function () {
+    // when
+    const command = deserializer(params, body);
+
+    // then
+    expect(command).to.be.deep.equal({
+      monthId: params.monthId,
+      name: body.name,
+      initialBalance: body.initialBalance,
+    });
+  });
+});
