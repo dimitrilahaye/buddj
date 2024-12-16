a143e5f68b3138d2a44b2d5095acc3d22491dcb8
Dimitri Lahaye <contact@dimitrilahaye.net>
2024-12-16 16:56:58 +0100

    chore(front): change output-native rule

--- front-v2/eslint.config.js
+++ front-v2/eslint.config.js
@@ -14,6 +14,7 @@ module.exports = tseslint.config(
     ],
     processor: angular.processInlineTemplates,
     rules: {
+      "@angular-eslint/no-output-native": ["warn"],
       "@angular-eslint/directive-selector": [
         "error",
         {
