7ba5e3c15fca57d77ccd565d06008d96cc69ebbe
Dimitri Lahaye <contact@dimitrilahaye.net>
2024-12-28 13:33:38 +0100

    feat(front): refund creation form + service call

--- front-v2/src/app/components/refunds/projects.component.spec.ts
+++ front-v2/src/app/components/refunds/projects.component.spec.ts
@@ -4,6 +4,9 @@ import { ProjectsComponent } from './projects.component';
 import { ActivatedRoute, Router } from '@angular/router';
 import { PROJECT_STORE } from '../../stores/projects/projects.store.interface';
 import { ProjectStore } from '../../stores/projects/projects.store';
+import { PROJECTS_SERVICE } from '../../services/projects/projects.service.interface';
+import { TOASTER_SERVICE } from '../../services/toaster/toaster.service.interface';
+import { ToasterService } from '../../services/toaster/toaster.service';
 
 describe('ProjectsComponent', () => {
   let component: ProjectsComponent;
@@ -15,6 +18,9 @@ describe('ProjectsComponent', () => {
       },
     },
   };
+  const projectServiceStub = {
+    create: jasmine.createSpy(),
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -23,6 +29,8 @@ describe('ProjectsComponent', () => {
         Router,
         { provide: ActivatedRoute, useValue: activatedRouteStub },
         { provide: PROJECT_STORE, useClass: ProjectStore },
+        { provide: PROJECTS_SERVICE, useValue: projectServiceStub },
+        { provide: TOASTER_SERVICE, useClass: ToasterService },
       ],
     }).compileComponents();
 
--- front-v2/src/app/components/refunds/projects.component.ts
+++ front-v2/src/app/components/refunds/projects.component.ts
@@ -1,5 +1,16 @@
-import { Component, Inject, OnInit, signal, Signal } from '@angular/core';
-import { Category } from '../../services/projects/projects.service.interface';
+import {
+  Component,
+  Inject,
+  model,
+  OnInit,
+  signal,
+  Signal,
+} from '@angular/core';
+import ProjectsServiceInterface, {
+  Category,
+  CreateCommand,
+  PROJECTS_SERVICE,
+} from '../../services/projects/projects.service.interface';
 import { ActivatedRoute, Router } from '@angular/router';
 import ProjectStoreInterface, {
   PROJECT_STORE,
@@ -7,6 +18,12 @@ import ProjectStoreInterface, {
 import { Project } from '../../models/project.model';
 import { HeaderBackButtonComponent } from '../header-back-button/header-back-button.component';
 import { DesignSystemModule } from '../../design-system/design-system.module';
+import { finalize } from 'rxjs';
+import ToasterServiceInterface, {
+  TOASTER_SERVICE,
+} from '../../services/toaster/toaster.service.interface';
+
+type SlidingModal = 'creation' | 'update' | 'numpad';
 
 @Component({
   selector: 'app-projects',
@@ -18,11 +35,18 @@ import { DesignSystemModule } from '../../design-system/design-system.module';
 export class ProjectsComponent implements OnInit {
   category: Category | undefined;
   projects: Signal<Project[] | null> = signal(null);
+  openSlidingModals: Set<SlidingModal> = new Set<SlidingModal>();
+  creatingProject = model<CreateCommand | null>(null);
+  isLoading = false;
+  updatingProject = model<Project | null>(null);
 
   constructor(
     private readonly route: ActivatedRoute,
     private readonly router: Router,
-    @Inject(PROJECT_STORE) private readonly store: ProjectStoreInterface
+    @Inject(PROJECT_STORE) private readonly store: ProjectStoreInterface,
+    @Inject(PROJECTS_SERVICE)
+    private readonly service: ProjectsServiceInterface,
+    @Inject(TOASTER_SERVICE) private readonly toaster: ToasterServiceInterface
   ) {}
 
   ngOnInit(): void {
@@ -36,7 +60,7 @@ export class ProjectsComponent implements OnInit {
   }
 
   hasProjects() {
-    return this.projects() === null || this.projects()!.length === 0;
+    return this.projects() !== null && this.projects()!.length > 0;
   }
 
   get tipsLabel() {
@@ -49,5 +73,86 @@ export class ProjectsComponent implements OnInit {
     return this.category === 'refund'
       ? 'info/no-refund-yet.png'
       : 'info/no-saving-yet.png';
+  }
+
+  get createButtonLabel() {
+    return this.category === 'refund'
+      ? 'Ajouter ce remboursement'
+      : 'Ajouter cette économie';
+  }
+
+  get toasterCreationMessage() {
+    return this.category === 'refund'
+      ? 'Votre remboursement a été crée !'
+      : 'Votre économie a été crée !';
+  }
+
+  closeSlidingModal(event: Event, modal: SlidingModal) {
+    event.stopPropagation();
+    this.openSlidingModals.delete(modal);
+  }
+
+  openSlidingModal(modal: SlidingModal) {
+    switch (modal) {
+      case 'creation':
+        this.creatingProject.set({
+          category: this.category!,
+          name: '',
+          target: 0,
+        });
+        break;
+    }
+    this.openSlidingModals.add(modal);
+  }
+
+  slidingModalIsOpen(modal: SlidingModal): boolean {
+    return this.openSlidingModals.has(modal);
+  }
+
+  updateCreatingProjectName(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.creatingProject.update((project) => {
+      if (project === null) {
+        return project;
+      }
+      return {
+        ...project,
+        name: value,
+      };
+    });
+  }
+
+  updateCreatingProjectTarget(target: string) {
+    const value = Number(target);
+    this.creatingProject.update((project) => {
+      if (project === null) {
+        return project;
+      }
+      return {
+        ...project,
+        target: value,
+      };
+    });
+    this.openSlidingModals.delete('numpad');
+  }
+
+  createProject(event: Event) {
+    event.stopPropagation();
+    if (this.creatingProject() === null) {
+      return;
+    }
+    const { name, target, category } = this.creatingProject()!;
+    this.isLoading = true;
+    this.service
+      .create({ name, target, category })
+      .pipe(
+        finalize(() => {
+          this.isLoading = false;
+          this.closeSlidingModal(event, 'creation');
+        })
+      )
+      .subscribe(() => {
+        this.toaster.success(this.toasterCreationMessage);
+      });
   }
 }
